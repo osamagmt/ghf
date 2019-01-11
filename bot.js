3045ba2cbce49751f97694124abf857597663413
discord <43854305+hgfhjtfuy@users.noreply.github.com>
2019-01-11 16:10:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2977,6 +2977,36 @@ client.on("guildMemberAdd", member => {
       });
 
 
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('message', message => {
+if(!message.channel.guild) return;
+  if(message.content.startsWith(prefix + 'sr')) {
+      let role = message.guild.roles.find('name', 'Rainbow ')
+    if(role) return message.channel.send(`يوجد بلفعل رتبه موجوده ضع البوت فوق الرتبه`)
+  if(!role){
+    rainbow =  message.guild.createRole({
+   name: "Rainbow ",//اسم الرتبه 
+   color: "#000000",//الون الاساسي للرنبو 
+   permissions:[]//الرتبه المسموح بيها للرنبو  مثال MANAGE_ROLES ADMINISTRATOR   
+ //نهايه الكود هنا
+})
+
+}
+message.channel.send('تم اعداد رتبه الرنبو بنجاح 🌈')//if the step completed
+}})
+
+client.on('ready', () => {//لا تغير شي هنا
+  setInterval(function(){//Codes Server
+      client.guilds.forEach(g => {//Codes Server
+                  var role = g.roles.find('name', 'Rainbow ');//اسم رتبه رنبو 
+                  if (role) {//Codes Server
+                      role.edit({color : "RANDOM"});//Codes Server
+                  };
+      });//Codes Server
+  }, 1000);//سرعه تغير الالوان
+})//Codes Server
 
 
 //MHSTR END NOW THIS IS END
