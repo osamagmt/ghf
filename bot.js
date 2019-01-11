bf5762b02adda3e53898f02cc1109f61e1471756
discord <43854305+hgfhjtfuy@users.noreply.github.com>
2019-01-11 16:19:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3009,5 +3009,44 @@ client.on('ready', () => {//لا تغير شي هنا
 })//Codes Server
 
 
+client.on("message", msg => { //Toxic Codes CopyRight & Me 92.♥#0092
+  if(msg.author.bot) return;
+  if(msg.channel.type === 'dm') return;
+let prefix = '!'; //البرفكس
+let msgarray = msg.content.split(" ");
+let cmd = msgarray[0];
+let args = msgarray.slice(1);
+if(cmd === `${prefix}warn`){//الامر
+  
+  
+
+  let rUser = msg.guild.member(msg.mentions.users.first() || msg.guild.members.get(args[0]));
+if(!rUser) return msg.channel.send("Couldn't find users.");
+    let reason = args.join(" ").slice(22);
+
+    let reportembed = new Discord.RichEmbed()
+    .setDescription("Warn")
+    .setColor("BLACK")
+    .addField("Warn User", `${rUser} with ID: ${rUser.id}`)
+    .addField("Warn By", `${msg.author} with ID: ${msg.author.id}`)
+    .addField("Channel", msg.channel)
+    .addField("Time", msg.createdAt)
+    .addField("Reason",`${reason}`)
+    
+    
+    let reportchannel = msg.guild.channels.find(`log`,"اسم الروم"); //حط هنا اسم الروم الي يوريك بعض المعلومات
+    if(!reportchannel) return msg.channel.send("Couldn't find `اسم الروم` channel. "); //ط هنا اسم الروم الي يوريك بعض المعلومات
+    
+    msg.delete().catch(O_o=>{});
+    reportchannel.send(reportembed);
+    let role = msg.guild.roles.find(`prox bot`, 'اسم الرتبة'); //حط هنا اسم الرتبة
+    if(!role) return msg.guild.channel.send("Could't find `اسم الرتبة` role."); //حط هنا اسم الرتبة
+    rUser.addRole(role);
+    
+        return;
+    }
+    }); //Toxic Codes
+
+
 //MHSTR END NOW THIS IS END
 client.login(process.env.BOT_TOKEN);
